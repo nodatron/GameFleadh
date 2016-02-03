d7569d926adb2a2dc4de8c01f1005e8061ecf9bf
Niall O Donnell <nodatron@gmail.com>
2016-02-03 17:17:17 +0000

    Proect made

---
+++ android/src/com/finalstand/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.finalstand.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.finalstand.game.FinalStand;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FinalStand(), config);
+	}
+}
---
+++ desktop/src/com/finalstand/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.finalstand.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.finalstand.game.FinalStand;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FinalStand(), config);
+	}
+}
