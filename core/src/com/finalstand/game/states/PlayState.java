626a62eeb3979621ab56f3c84831b7bdc4e9889e
Niall O Donnell <nodatron@gmail.com>
2016-02-11 17:37:18 +0000

    Added orthographic camera and viewport

--- core/src/com/finalstand/game/states/PlayState.java
+++ core/src/com/finalstand/game/states/PlayState.java
@@ -3,7 +3,10 @@ package com.finalstand.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.finalstand.game.FinalStand;
 
 /**
@@ -11,12 +14,16 @@ import com.finalstand.game.FinalStand;
  */
 public class PlayState implements Screen {
 
+    private OrthographicCamera gameCam;
+    private Viewport viewport;
     private FinalStand game;
     Texture texture;
 
     public PlayState(FinalStand game) {
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+        texture = new Texture("background.jpg");
+        gameCam = new OrthographicCamera();
+        viewport = new FitViewport(1280, 720, gameCam);
     }
 
     @Override
@@ -28,6 +35,7 @@ public class PlayState implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -35,7 +43,7 @@ public class PlayState implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
