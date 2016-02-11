e851c99c5e1621f97c5757cf170baa0786402567
Keith <c14337491@mydit.ie>
2016-02-11 17:05:18 +0000

    added aoe sprites

--- core/src/com/finalstand/game/towers/AOETower.java
+++ core/src/com/finalstand/game/towers/AOETower.java
@@ -1,5 +1,7 @@
 package com.finalstand.game.towers;
 
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  * Created by Keith on 09/02/2016.
  */
@@ -9,6 +11,10 @@ public class AOETower extends Tower{
     public AOETower(float x, float y)
     {
         super(x, y);
+
+        level1 = new Texture("aoe_level1.png");
+        level2 = new Texture("aoe_level2.png");
+        level3 = new Texture("aoe_level3.png");
     }
 
     public void update()
