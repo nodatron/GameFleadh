f403d531c6fbe065e42142a3af7763475b144919
Carl Kavanagh <alterego01010@gmail.com>
2016-02-16 22:34:20 +0000

    Added all Trap classes

---
+++ core/src/com/finalstand/game/traps/Glue.java
@@ -0,0 +1,12 @@
+package com.finalstand.game.traps;
+
+/**
+ * Created by Carl on 16/02/2016.
+ */
+public class Glue extends Trap
+{
+    Glue(float x, float y)
+    {
+        super(x, y);
+    }
+}
---
+++ core/src/com/finalstand/game/traps/Oil.java
@@ -0,0 +1,12 @@
+package com.finalstand.game.traps;
+
+/**
+ * Created by Carl on 16/02/2016.
+ */
+public class Oil extends Trap
+{
+    Oil(float x, float y)
+    {
+        super(x, y);
+    }
+}
