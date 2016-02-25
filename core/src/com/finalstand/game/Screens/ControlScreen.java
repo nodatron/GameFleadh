ff044774ab9285d8600e889cf4577be7ade850ee
Niall O Donnell <nodatron@gmail.com>
2016-02-25 21:16:07 +0000

    Added more buttons and basic screens

--- core/src/com/finalstand/game/Screens/ControlScreen.java
+++ core/src/com/finalstand/game/Screens/ControlScreen.java
@@ -5,44 +5,44 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.finalstand.game.FinalStand;
-import com.finalstand.game.buttons.Button;
-import com.finalstand.game.buttons.ControlButton;
+import com.finalstand.game.buttons.BackButton;
 import com.finalstand.game.buttons.PlayButton;
 
 /**
  * Created by Niall PC on 25/02/2016.
  */
-public class SplashScreen implements Screen{
+public class ControlScreen implements Screen {
 
     private final FinalStand game;
+    private final Screen lastScreen;
     private Viewport viewport;
     private static OrthographicCamera gameCam;
     private Texture background;
 
     private PlayButton playButton;
-    private ControlButton controlButton;
+    private BackButton backButton;
 
     private static boolean playButtonPressed;
-    private static boolean controlButtonPressed;
-    private static boolean quitButtonPressed;
+    private static boolean backButtonPressed;
 
-    public SplashScreen(FinalStand game) {
+    public ControlScreen(FinalStand game, Screen lastScreen) {
         this.game = game;
+        this.lastScreen = lastScreen;
         gameCam = new OrthographicCamera();
         viewport = new FitViewport(FinalStand.V_WIDTH / FinalStand.PPM, FinalStand.V_HEIGHT / FinalStand.PPM, gameCam);
         gameCam.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
-        background = new Texture("screens/title.png");
+        background = new Texture("screens/menu.png");
 
         playButton = new PlayButton("screens/playbutton.png", 300 / FinalStand.PPM, 100 / FinalStand.PPM, 50 / FinalStand.PPM, 50 / FinalStand.PPM);
-        controlButton = new ControlButton("screens/controlbutton.png", 300 / FinalStand.PPM, 25 / FinalStand.PPM, 50 / FinalStand.PPM, 50 / FinalStand.PPM);
+        backButton = new BackButton("screens/backbutton.png", 300 / FinalStand.PPM, 50 / FinalStand.PPM, 50 / FinalStand.PPM, 50 / FinalStand.PPM);
 
         playButtonPressed = false;
-        controlButtonPressed = false;
-        quitButtonPressed = false;
+        backButtonPressed = false;
     }
 
     @Override
@@ -53,9 +53,9 @@ public class SplashScreen implements Screen{
     @Override
     public void render(float delta) {
         playButton.update();
-        controlButton.update();
+        backButton.update();
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.setProjectionMatrix(gameCam.combined);
@@ -63,21 +63,21 @@ public class SplashScreen implements Screen{
 //        playButton.getButtonSprite().draw(game.batch);
         game.batch.draw(background, 0, 0, 800 / FinalStand.PPM, 400 / FinalStand.PPM);
         game.batch.draw(playButton.getButtonTexture(), playButton.getPosition().x, playButton.getPosition().y, playButton.getWidth(), playButton.getHeight());
-        game.batch.draw(controlButton.getButtonTexture(), controlButton.getPosition().x, controlButton.getPosition().y, controlButton.getWidth(), controlButton.getHeight());
+        game.batch.draw(backButton.getButtonTexture(), backButton.getPosition().x, backButton.getPosition().y, backButton.getWidth(), backButton.getHeight());
         game.batch.end();
 
         if(playButtonPressed) {
             game.setScreen(new PlayScreen(game));
         }
 
-        if(controlButtonPressed) {
-            game.setScreen(new ControlScreen(game, this));
+        if(backButtonPressed) {
+            game.setScreen(lastScreen);
         }
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+
     }
 
     @Override
@@ -105,17 +105,11 @@ public class SplashScreen implements Screen{
         System.out.println("here");
     }
 
-    public static void controlButtonPressed() {
-        controlButtonPressed = true;
-        System.out.println("here");
-    }
-
-    public static void quitButtonPressed() {
-        quitButtonPressed = true;
+    public static void backButtonPressed() {
+        backButtonPressed = true;
     }
 
     public static Vector3 getWorldMousePos() {
         return gameCam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
     }
-
 }
